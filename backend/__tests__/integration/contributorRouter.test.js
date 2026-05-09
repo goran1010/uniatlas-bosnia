a38ec74c2ecb9183f08e46ee2d49f9126d679488
Goran Jovic <goran1010jovic@gmail.com>
2026-05-09 13:06:48 +0200

    test: add tests for contribution router

--- backend/__tests__/integration/contributorRouter.test.js
+++ backend/__tests__/integration/contributorRouter.test.js
@@ -84,3 +84,49 @@ describe("Contributor Router - DELETE /users/contribution/postal-codes", () => {
     await usersModel.deleteUser({ id: loginResponse.body.data.id });
   });
 });
+
+describe("Contributor Router - GET /users/contribution/pending-changes/postal-codes", () => {
+  test("Responds with status 200 and message if pending changes retrieved successfully", async () => {
+    const agent = request.agent(app);
+    const loginResponse = await createAndLoginUser(agent);
+
+    const response = await agent.get(
+      "/users/contribution/pending-changes/postal-codes",
+    );
+
+    expect(response.header["content-type"]).toMatch(/json/);
+
+    expect(response.body.message).toBe(
+      "Pending changes retrieved successfully.",
+    );
+    expect(response.status).toBe(200);
+
+    await usersModel.deleteUser({ id: loginResponse.body.data.id });
+  });
+});
+
+describe("Contributor Router - DELETE /users/contribution/pending-changes/postal-codes", () => {
+  test("Responds with status 200 and message if pending change deleted successfully", async () => {
+    const agent = request.agent(app);
+    const loginResponse = await createAndLoginUser(agent);
+
+    const pendingChange = await pendingChangesPostalCodeModel.create({
+      userId: loginResponse.body.data.id,
+      code: 12345,
+      typeOfChange: "CREATE",
+      city: "Test",
+      post: "BH_POSTA",
+    });
+
+    const response = await agent
+      .delete("/users/contribution/pending-changes/postal-codes")
+      .send({ id: pendingChange.id });
+
+    expect(response.header["content-type"]).toMatch(/json/);
+
+    expect(response.body.message).toBe("Pending change deleted successfully.");
+    expect(response.status).toBe(200);
+
+    await usersModel.deleteUser({ id: loginResponse.body.data.id });
+  });
+});
