94ce63ee3f46d10879922de0f428e267cd67ee2f
Goran Jovic <goran1010jovic@gmail.com>
2026-05-14 13:39:27 +0200

    test: add more tests for Home component

--- frontend/__tests__/components/Home/Home.test.jsx
+++ frontend/__tests__/components/Home/Home.test.jsx
@@ -6,19 +6,70 @@ import { MemoryRouter } from "react-router-dom";
 
 describe("Home component", () => {
   test("render component", async () => {
-    const contextValue = {};
-
     render(
-      <NotificationContext value={contextValue}>
-        <MemoryRouter>
-          <Home />
-        </MemoryRouter>
-      </NotificationContext>,
+      <MemoryRouter>
+        <Home />
+      </MemoryRouter>,
     );
     const linkElement = await screen.findByRole("heading", {
       name: /Bosnia Lens/i,
       level: 1,
     });
     expect(linkElement).toBeInTheDocument();
+  });
+
+  test("render available data section", async () => {
+    render(
+      <MemoryRouter>
+        <Home />
+      </MemoryRouter>,
+    );
+    const linkElement = await screen.findByRole("heading", {
+      name: /Available data/i,
+      level: 2,
+    });
+    expect(linkElement).toBeInTheDocument();
+  });
+
+  test("render contributing section", async () => {
+    render(
+      <MemoryRouter>
+        <Home />
+      </MemoryRouter>,
+    );
+    const linkElement = await screen.findByRole("heading", {
+      name: /Contributing/i,
+      level: 2,
+    });
+    expect(linkElement).toBeInTheDocument();
+  });
+
+  test("render get started section", async () => {
+    render(
+      <MemoryRouter>
+        <Home />
+      </MemoryRouter>,
+    );
+    const linkElement = await screen.findByRole("heading", {
+      name: /Get started/i,
+      level: 2,
+    });
+    expect(linkElement).toBeInTheDocument();
+  });
+
+  test("render get started section with links", async () => {
+    render(
+      <MemoryRouter>
+        <Home />
+      </MemoryRouter>,
+    );
+    const linkElement = await screen.findByRole("link", {
+      name: /REST API/i,
+    });
+    expect(linkElement).toBeInTheDocument();
+    const linkElement2 = await screen.findByRole("link", {
+      name: /Postal Codes/i,
+    });
+    expect(linkElement2).toBeInTheDocument();
   });
 });
