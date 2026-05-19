fe81bde424e1015d2fc4c8cfc716d85626b6c5c7
Goran Jovic <goran1010jovic@gmail.com>
2026-05-19 15:57:42 +0200

    feat: add React Helmet to long wait info

--- frontend/src/utils/longWaitInfo.jsx
+++ frontend/src/utils/longWaitInfo.jsx
@@ -1,25 +1,36 @@
 import { useContext } from "react";
 import { LanguageContext } from "../contextData/LanguageContext";
+import { Helmet } from "react-helmet-async";
 
 function LongWaitInfo({ serverIsDown }) {
   const { t } = useContext(LanguageContext);
 
   if (serverIsDown) {
     return (
-      <main className="overlay-screen fixed w-full h-full flex items-center justify-center p-2 md:p-4 lg:p-6 xl:p-8 2xl:p-10">
-        <div className="status-banner status-banner--error p-2 rounded">
-          <p className="font-bold text-center">{t("longWait.unreachable")}</p>
-        </div>
-      </main>
+      <>
+        <Helmet>
+          <title>{`${t("title.serverDown")} | ${t("title.app")}`}</title>
+        </Helmet>
+        <main className="overlay-screen fixed w-full h-full flex items-center justify-center p-2 md:p-4 lg:p-6 xl:p-8 2xl:p-10">
+          <div className="status-banner status-banner--error p-2 rounded">
+            <p className="font-bold text-center">{t("longWait.unreachable")}</p>
+          </div>
+        </main>
+      </>
     );
   }
 
   return (
-    <main className="overlay-screen fixed w-full h-full flex items-center justify-center p-2 md:p-4 lg:p-6 xl:p-8 2xl:p-10">
-      <div className="status-banner status-banner--info p-2 rounded">
-        <p className="font-bold text-center">{t("longWait.wakingUp")}</p>
-      </div>
-    </main>
+    <>
+      <Helmet>
+        <title>{`${t("title.wakingUp")} | ${t("title.app")}`}</title>
+      </Helmet>
+      <main className="overlay-screen fixed w-full h-full flex items-center justify-center p-2 md:p-4 lg:p-6 xl:p-8 2xl:p-10">
+        <div className="status-banner status-banner--info p-2 rounded">
+          <p className="font-bold text-center">{t("longWait.wakingUp")}</p>
+        </div>
+      </main>
+    </>
   );
 }
 
