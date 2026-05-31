1534e7867b54473a8d147491d5bb8bfa9ee051ae
Goran Jovic <goran1010jovic@gmail.com>
2026-05-31 12:52:01 +0200

    feat: improve layout and styling of language switcher

--- frontend/src/components/Navbar/LanguageSwitcher.jsx
+++ frontend/src/components/Navbar/LanguageSwitcher.jsx
@@ -1,8 +1,9 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { RootContext } from "../../contextData/RootContext";
 import { Select } from "../sharedComponents/Select";
 
-function LanguageSwitcher({ language, setLanguage }) {
+function LanguageSwitcher({ setLanguage }) {
+  const [languageLabel, setLanguageLabel] = useState("");
   const { addNotification } = useContext(RootContext);
   const { t } = useContext(RootContext);
 
@@ -10,6 +11,7 @@ function LanguageSwitcher({ language, setLanguage }) {
     const nextLanguage = e.target.value;
     if (!nextLanguage) return;
     setLanguage(nextLanguage);
+    setLanguageLabel(nextLanguage);
 
     addNotification({
       type: "info",
@@ -27,10 +29,13 @@ function LanguageSwitcher({ language, setLanguage }) {
     <Select
       id="language-switcher"
       aria-label={t("language.switchAria")}
-      value={language}
+      value={languageLabel}
       onChange={handleLanguageChange}
       className="whitespace-normal text-center break-all h-full py-2 sm:min-w-38 w-full sm:w-auto relative inline-flex items-center justify-center rounded-md p-1 text-sm font-semibold bg-(--surface-1) text-(--text-primary) border border-(--border-color) shadow-(--card-shadow-soft) hover:shadow-(--card-shadow)"
     >
+      <option className="font-bold" value="">
+        {t("language.select")}
+      </option>
       <option value="system">{t("language.system")}</option>
       <option value="en">{t("language.english")}</option>
       <option value="sr">{t("language.serbian")}</option>
