37dce79d851a1c953c1a44bede46cb50e54a052d
Goran Jovic <goran1010jovic@gmail.com>
2026-06-04 10:23:48 +0200

    chore: update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,29 @@
-import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
+/// <reference types="vitest/config" />
+
+import { defineConfig } from "vite";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import babel from "@rolldown/plugin-babel";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    babel({ presets: [reactCompilerPreset()] })
+    babel({ presets: [reactCompilerPreset()] }),
+    tailwindcss(),
   ],
-})
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./__tests__/setup.js",
+    coverage: {
+      include: ["src/**/*.{ts,tsx}"],
+      exclude: [
+        "src/main.tsx",
+        "**/*.test.{ts,tsx}",
+        "**/*.config.{ts,tsx}",
+        "**/node_modules/**",
+      ],
+    },
+  },
+});
