923cd36a36cff1f627ca03ea94afebe9079dcd83
Goran Jovic <goran1010jovic@gmail.com>
2026-06-15 13:32:12 +0200

    refactor: initial commit for backend refactor to TypeScript

--- backend/prisma/migrations/20260224180009_add_contributor_role/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "users" ADD COLUMN     "isContributor" BOOLEAN NOT NULL DEFAULT false;
--- backend/prisma/migrations/20260502102035_remove_foreign_key_pending_code/migration.sql
+++
@@ -1,2 +0,0 @@
--- DropForeignKey
-ALTER TABLE "pending_changes_postal_code" DROP CONSTRAINT "pending_changes_postal_code_code_fkey";
