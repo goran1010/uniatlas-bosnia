95312e7d12075b4a194127cdd8296337aa3ffc60
Goran Jovic <goran1010jovic@gmail.com>
2026-06-25 11:02:19 +0200

    test: add types to envCheck.test.ts

--- backend/__tests__/envCheck.test.ts
+++ backend/__tests__/envCheck.test.ts
@@ -2,16 +2,16 @@ import { describe, test, expect, vi } from "vitest";
 
 describe("process.env variable missing or undefined", () => {
   test("envCheck should throw an error if process.env.DATABASE_URL is missing", async () => {
-    const originalDatabaseUrl = process.env.DATABASE_URL;
+    const originalDatabaseUrl = process.env["DATABASE_URL"];
 
-    delete process.env.DATABASE_URL;
+    delete process.env["DATABASE_URL"];
     vi.resetModules();
 
-    await expect(import("../src/config/env.js")).rejects.toThrowError(
+    await expect(import("../src/config/env.js")).rejects.toThrow(
       "Missing environment variable: DATABASE_URL",
     );
 
-    process.env.DATABASE_URL = originalDatabaseUrl;
+    process.env["DATABASE_URL"] = originalDatabaseUrl;
     vi.resetModules();
   });
 });
